6815be0c02bbb21ed59c5ebc494b236b90de1c8b
OhyaYouki <88011584+OhyaYouki@users.noreply.github.com>
2022-01-31 16:40:28 +0900

    エラー文の表示できなかった

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,31 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all.order(id:"DESC")
+    @post = Post.new
   end
 
   #def new
   #end
 
   def create
-    post = Post.create(content: params[:content])
-    render json:{post: post}
+    @post = Post.new(params_content)
+    @post.save
+    render json:{post: @post}
+    
+    # エラー文を出したかった
+    # binding.pry
+    # if @post.save
+    #   # binding.pry
+    # else
+    #   # binding.pry
+    #   @posts = Post.all.order(id:"DESC")
+    #   render :index
+    # end
+     
+  end
+
+  private
+  def params_content
+    params.require(:post).permit(:content)
   end
 end
