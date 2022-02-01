3c7f87e85d82fdb51411daedb71c68f95d3a359a
OhyaYouki <88011584+OhyaYouki@users.noreply.github.com>
2022-02-02 08:52:27 +0900

    同期通信のいいね、いいね削除

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,9 +2,19 @@ class LikesController < ApplicationController
 
   def create
     # binding.pry
-    Like.create(user_id:current_user.id,post_id:params[:post_id])
-    
-    redirect_to root_path
+    @post = Post.find(params[:post_id])
+
+    if @post.likes.present? && current_user.likes.present?
+      like = Like.where(user_id:current_user.id,post_id:params[:post_id])
+      # binding.pry
+      # destroyだとエラーになる
+      like.destroy_all
+      redirect_to post_path(@post.id)
+    else
+      Like.create(user_id:current_user.id,post_id:params[:post_id])
+      redirect_to post_path(@post.id)
+    end
+
   end
 
   def destroy
