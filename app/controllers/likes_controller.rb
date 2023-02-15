0bb3561101e9c10e58bdec0fbde44e7309647e84
OhyaYouki <88011584+OhyaYouki@users.noreply.github.com>
2023-02-15 21:35:25 +0900

    ユーザー詳細ページでいいねをした時の実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -19,11 +19,59 @@ class LikesController < ApplicationController
     # @をつけないようにするといらなくなる
   end
 
+
+  # 以下users/show/html.erbのあるユーザーがいいねした投稿に対していいねをする、いいねを外した時の挙動
+    # 自分ではないユーザーの詳細ページでは
+    # いいねや、いいねを外すことができ
+
+    # 自分の詳細ページでいいねを外した場合
+    # 非同期でいいねの投稿一覧から消えるような実装
+
+
+  # やり直し。ルーティングをuserとネストしたところから
+
+
+
+  def user_show_create
+    # binding.pry
+    # ユーザー詳細ページのuser情報
+    @user = User.find(params[:user_id])
+
+    # 表示されている投稿の情報
+    post = Like.find(params[:id]).post
+
+    # 上記の投稿に対してログイン者がいいねした情報
+    Like.create(post_id:post.id,user_id:current_user.id)
+
+    @likes = @user.likes
+
+
+    # Like.create(like_params)
+    # @like = Like.where(like_params)
+    # post = Post.find(params[:post_id])
+    # @user = post.user
+    # @likes = @user.likes
+  end
+
   def user_show_destroy
-    like = Like.find(params[:id])
+    # binding.pry
+
+    # ユーザー詳細ページのuser情報
+    @user = User.find(params[:user_id])
+
+    # 表示されている投稿の情報
+    post = Like.find(params[:id]).post
+
+    # 上記の投稿に対してログイン者がいいねした情報
+    like = Like.find_by(post_id:post.id,user_id:current_user.id)
     like.destroy
 
-    @user = User.find(params[:user_id])
+    @likes = @user.likes
+
+
+    # binding.pry
+    # @likes = Like.find(params[:id]).user.likes
+    # @user = User.find(params[:user_id])
     @likes = @user.likes
     # binding.pry
   end
@@ -34,9 +82,9 @@ class LikesController < ApplicationController
     params.permit(:post_id).merge(user_id:current_user.id)
   end
 
-  def user_show_params
-    params.permit(:post_id).merge(user_id:current_user.id)
-  end
+  # def user_show_params
+  #   params.permit(:user_id).merge(user_id:current_user.id)
+  # end
 
   def set_like
     @post = Post.find(params[:post_id])
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,15 @@ Rails.application.routes.draw do
 
   resources :posts,only: [:index,:create,:show]  do
     resources :likes, only: [:create,:destroy]
+
+    # 以下はダメ。上とかぶる
+    # post 'likes', to: 'likes#user_show_create'
+    # delete 'likes/:id', to: 'likes#user_show_destroy'
   end
 
   resources :users,only: :show do
     # resources :likes, only: [:destroy]
+    post 'likes/:id', to: 'likes#user_show_create'
     delete 'likes/:id', to: 'likes#user_show_destroy'
   end
 end
